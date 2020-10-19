e3fca85bde15e961a870094888b7322c285abd18
Shashank Bezgoan <shashank.bezgoan@bt.com>
2020-10-19 07:23:17 +0100

    Documentation updated.

---
+++ Cormen/__init__.py
@@ -0,0 +1,15 @@
+__doc__ = """
+
+    Aspiration is to implement and document all algorithms in 
+    Introduction to Algorithms book by 
+    
+    1. Thomas H. Cormen
+    2. Charles E Leiserson
+    3. Ronald L Rivest
+    4. Clifford Stein
+
+
+.. _Introduction to Algorithms: 
+https://www.amazon.co.uk/Introduction-Algorithms-Thomas-H-Cormen/dp/0262033844/ref=sr_1_2?dchild=1&keywords=The+Algorithms&qid=1603013507&sr=8-2
+
+"""
