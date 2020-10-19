5bdf37d6efdafe49c052b0ee852234fcfc816557
Shashank Bezgoan <shashank.bezgoan@bt.com>
2020-10-19 21:39:18 +0100

    documentation for insertion_sort and linear_search added

---
+++ Cormen/Ch02/__init__.py
@@ -0,0 +1,8 @@
+__doc__= """
+
+Chapter 2 : 
+1. Deals with insertion sort, linear search, selection sort, merge sort algorithms.
+2. Analysis of Algorithms.
+
+
+"""
--- Cormen/Ch02/linear_search.py
+++ Cormen/Ch02/linear_search.py
@@ -1,6 +1,28 @@
 
 
 def linear_search(A, key):
+    """Find a key in a list of integers.
+
+            Parameters
+            ----------
+            A : list
+                List of integers.
+            key : int
+
+
+            Returns
+            -------
+            int
+                index of the key within the list A.
+
+
+            Complexity
+            ----------
+
+            Best Case : O(1)
+            Worst Case : O(n)
+
+    """
     index = None
     for i in range(1,len(A)):
         if key == A[i]:
