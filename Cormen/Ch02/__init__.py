4635d3e8c46d450e185479e3bf8c58b28d805713
Shashank Bezgoan <shashank.bezgoan@bt.com>
2020-10-19 22:07:28 +0100

    Revert "documentation for insertion_sort and linear_search added"
    This reverts commit 5bdf37d6efdafe49c052b0ee852234fcfc816557.

--- Cormen/Ch02/__init__.py
+++
@@ -1,8 +0,0 @@
-__doc__= """
-
-Chapter 2 : 
-1. Deals with insertion sort, linear search, selection sort, merge sort algorithms.
-2. Analysis of Algorithms.
-
-
-"""
--- Cormen/Ch02/insertion_sort.py
+++ Cormen/Ch02/insertion_sort.py
@@ -1,4 +1,4 @@
-def _insertion_sort_desc(A):
+def insertion_sort_desc(A):
     for j in range(1, len(A)):
         key = A[j]
         i = j - 1
@@ -8,7 +8,7 @@ def _insertion_sort_desc(A):
         A[i + 1] = key
     return A
 
-def _insertion_sort_asc(A):
+def insertion_sort_asc(A):
 
     for j in range(1,len(A)):
         key = A[j]
@@ -19,31 +19,9 @@ def _insertion_sort_asc(A):
         A[i+1] = key
     return A
 
+
 def insertion_sort(A, order):
-    """Sorts a list of integers in ascending or descending order.
-
-        Parameters
-        ----------
-        A : list
-            List of integers to be sorted.
-        order : 0,1
-            0 is ascending order
-            1 is descending order.
-
-        Returns
-        -------
-        list
-            a list of sorted integers in the specified order.
-
-
-        Complexity
-        ----------
-
-        Best Case : O(n)
-        Worst Case : O(n^2)
-
-        """
     if order == 0:
-        return _insertion_sort_asc(A)
+        return insertion_sort_asc(A)
     elif order == 1:
-        return _insertion_sort_desc(A)
+        return insertion_sort_desc(A)
