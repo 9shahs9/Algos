6fe2893f16b71c9fed7edee7d20d684af0cf5663
Shashank Bezgoan <shashank.bezgoan@bt.com>
2020-11-25 15:39:29 +0000

    Chapter 4 max sub array problem using Divide and Conquer.

---
+++ Cormen/Ch04/max_sub_array.py
@@ -0,0 +1,76 @@
+"""
+Given an array of elements find the sub-array which has the largest sum of the entire array.
+
+E.g.
+Given ticker values of a stock at end of day for future in advance to trader,
+Can the trader decide to find which day to buy and which day to sell the stock on ?
+
+
+Ticker values : [100,113,110,85,105,102,86,63,81,101,94,106, 101, 79, 94, 90, 97]
+
+deltas : [-, 13,-3,-15,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
+
+Given the difference array which calculates the difference of stock value to its previous day, barring the first ticker value.
+Find the maximum-sub-array i.e. day which trader can buy the stock and sell it making maximum profit of the given future ticker values.
+
+Brute force algorithm :
+    is to find sum of any two of the N values and find the largest.
+    Solution has a run time of O(N^2)
+
+divide-and-conquer :
+    1) split the array into two :
+    2) find the max value in left sub-array and right sub-array independently
+    3) find the max value between min index of left-sub-array and max index of right-sub-array.
+    4) compare max-left, max-right and max-crossing-mid to find the max value.
+
+"""
+
+
+def find_max_crossing_subarray(A, low, mid, high):
+    '''
+    Combine method of the divide-and-conquer algorithm
+
+    :param A:  Array of integers
+    :param low: left low found by d&c left split.
+    :param mid:  split point of Array.
+    :param high: right high found by d&c right split.
+    :return:  returns low, high, max.
+    '''
+
+    left_sum = right_sum = float('-inf')
+    max_left = max_right = mid
+    sum = 0
+    for i in range(mid,low-1,-1):
+        sum = sum + A[i]
+        if sum > left_sum :
+            left_sum = sum
+            max_left = i
+    sum = 0
+    for j in range(mid+1, high+1, 1):
+        sum = sum + A[j]
+        if sum > right_sum:
+            right_sum = sum
+            max_right = j
+    return (max_left, max_right, left_sum + right_sum)
+
+
+
+def find_max_subarray(A, low, high):
+    'Base case of one element in the Array'
+    if high == low :
+        return (low, high, A[low])
+
+    'Dividing the problem in two sub-problems.'
+    mid = (int)((low+high) / 2)
+    (left_low, left_high, left_sum ) = find_max_subarray(A, low, mid)
+    (right_low, right_high, right_sum) = find_max_subarray(A, mid+1, high)
+
+    'Combine phase of divide-and-conquer'
+    (cross_low, cross_high, cross_sum) = find_max_crossing_subarray(A, low, mid, high)
+
+    if left_sum >= right_sum and left_sum >= cross_sum:
+        return (left_low, left_high, left_sum)
+    if right_sum >= left_sum and right_sum >= cross_sum:
+        return (right_low, right_high, right_sum)
+    return (cross_low, cross_high, cross_sum)
+
---
+++ tests/Cormen/Ch04/max_sub_array_test.py
@@ -0,0 +1,6 @@
+import Cormen.Ch04.max_sub_array as algos
+
+def test_find_max_subarray():
+    test_array = [13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,15,-4,7]
+    st_idx, end_idx, sum = algos.find_max_subarray(test_array,0,15)
+    assert sum == 43
